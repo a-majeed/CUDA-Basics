2eb0fb590a83d29a03475464605249e4cb7b8ce3
Azaan Majeed <azaan@ualberta.ca>
2021-12-06 16:28:41 -0700

    created funtion to add elements of array

--- cuda.cpp
+++ cuda.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 #include <math.h>
+
+// creating function to add elements of an array
+void add(int n, float *x, float *y){
+    
+    for (int i = 0; i < n; i++){
+        y[i] = x[i] + y[i];
+    }
+}
