21edcf578ec6f5f673d729de6893817f3a473617
Azaan Majeed <azaan@ualberta.ca>
2021-12-07 13:28:40 -0700

    changig add function to run computation over parallel threads

--- cuda2.cu
+++ cuda2.cu
@@ -6,7 +6,10 @@
 __global__
 void add(int n, float *x, float *y){
 
-    for (int i = 0; i < n; i++){
+    int index = threadIdx.x;
+    int stride = blockDim.x;
+
+    for (int i = index; i < n; i+= stride){
         y[i] = x[i] + y[i];
     }
 }
@@ -47,6 +50,4 @@ int main(void){
     
 
     return 0;
-
-
 }
